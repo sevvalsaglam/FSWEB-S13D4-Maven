da680b9d6c91e709e0c7cde34e02b02481e3a7ce
sevvalsaglam <sevvallsaglam@gmail.com>
2024-09-20 16:36:23 +0300

    sprint 13 gün 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,34 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point first =new Point(6,5);
+        Point second =new Point(3,1);
+
+        System.out.println("distance(0,0)= " + first.distance());
+
+        System.out.println("distance(second)= " + first.distance(second));
+
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+
+        Point point = new Point(0,0);
+
+        System.out.println("distance()= " + point.distance());
+
+        System.out.println("-----------------------------------------");
+
+        Player player=new Player("şevval",100, Weapon.AXE);
+        System.out.println("healthremaining: "+player.healthRemaining());
+
+        Player player2=new Player("selen",90,Weapon.SWORD);
+        System.out.println("healthremaining: "+player2.healthRemaining());
+
+        player.loseHealth(Integer.valueOf((int)(player2.getWeapon().getDamage()*player2.getWeapon().getAttackSpeed())));
+
+        System.out.println("healthremaining after damage: "+ player.healthRemaining());
+
+        player.restoreHealth(70);
+        System.out.println("healthremaining after restore: "+ player.healthRemaining());
+
     }
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,10 @@
+import org.example.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.example.Point;
-import org.example.Player;
 import org.example.Weapon;
 
 import java.lang.reflect.Field;
